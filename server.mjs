0121cf472b8bdd4810ab965274e42031e47194ca
hecterbonha <untungsoandryanto@gmail.com>
2021-11-29 17:08:54 +0700

    feat(static): add static public routes

--- server.mjs
+++ server.mjs
@@ -1,12 +1,22 @@
 import fastify from "fastify";
 import cors from "fastify-cors";
 import helmet from "fastify-helmet";
+import fstatic from "fastify-static";
+
+import path from "path";
 
 import { config } from "./config.mjs";
 import { router } from "./router/index.mjs";
 
 const server = fastify({
   logger: { level: `info` },
+});
+
+let __dirname = path.resolve(path.dirname(""));
+
+server.register(fstatic, {
+  root: path.join(__dirname, "public"),
+  prefix: "/public/",
 });
 
 server.register(helmet);
