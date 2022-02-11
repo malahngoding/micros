02aadcb8a505d9bb0321a8cafe2be9dd8376a09e
hecterbonha <untungsoandryant@gmail.com>
2022-02-11 18:49:26 +0700

    feat(register): flash card

--- controllers/flash-card.mjs
+++ controllers/flash-card.mjs
@@ -23,3 +23,33 @@ export const getFlashCardRanking = async (_, res) => {
   };
   return res.send(responseObject);
 };
+
+export const getCurrentUserFlashCardStatus = async (_, res) => {
+  const list = [];
+  [1, 2, 3, 4, 5, 6].map((item) => {
+    list.push({
+      rank: item,
+      userName: generateSlug(2, { format: "title" }),
+      avatar: `${item} ${new Date()}`,
+      score: `${randomstring.generate({
+        length: 4,
+        charset: "numeric",
+      })}`,
+    });
+  });
+  const responseObject = {
+    messages: `Hello FlashCard Stats`,
+    status: `OK`,
+    payload: {
+      stats: {
+        finishedGroupQuestion: 0.1,
+        answeredQuestion: 0.1,
+        skippedQuestion: 0.1,
+        correctAnswer: 0.1,
+        wrongAnswer: 0.1,
+        accuracy: 0.1,
+      },
+    },
+  };
+  return res.send(responseObject);
+};
--- router/flash-card.mjs
+++ router/flash-card.mjs
@@ -1,9 +1,16 @@
 import * as controllers from "../controllers/index.mjs";
+import * as middleware from "../middleware/index.mjs";
 
 export const flashCard = {
   getFlashCardRanking: {
     method: `GET`,
     url: `/getFlashCardRanking`,
     handler: controllers.getFlashCardRanking,
+  },
+  getCurrentUserFlashCardStatus: {
+    method: `POST`,
+    url: `/getCurrentUserFlashCardStatus`,
+    handler: controllers.getCurrentUserFlashCardStatus,
+    preHandler: [middleware.authenticated],
   },
 };
