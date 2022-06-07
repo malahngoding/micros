4da3be1cd7326c55633876a42466cb765a25ab99
hecterbonha <untungsoandryanto@gmail.com>
2022-06-07 17:19:31 +0700

    chore(): fix user generator

--- database/users.mjs
+++ database/users.mjs
@@ -1,25 +1,31 @@
 import { generateSlug } from "random-word-slugs";
 import randomstring from "randomstring";
+import Web3 from "web3";
 import { prisma } from "./prisma.mjs";
 
 export const initUsers = () => {
+  let web3 = new Web3();
   [1, 2, 3, 4, 5, 6].map(async (item) => {
     const dechiperedIdentification = "0.0.432156" + item;
     const provider = "HEDERA";
+    const eth = web3.utils.randomHex(4);
+    const randomSlug = () => {
+      return generateSlug(3, {
+        partsOfSpeech: ["adjective", "adjective", "noun"],
+        categories: {
+          adjective: ["color", "appearance", "quantity"],
+          noun: ["animals", "thing", "technology"],
+        },
+      });
+    };
     await prisma.user.create({
       data: {
         identification: `${dechiperedIdentification}__${provider}`,
-        userName: generateSlug(3, {
-          partsOfSpeech: ["adjective", "adjective", "noun"],
-          categories: {
-            adjective: ["color", "appearance", "quantity"],
-            noun: ["animals", "thing", "technology"],
-          },
-        }),
+        userName: eth,
         Profile: {
           create: {
-            name: `Test User ${item}`,
-            email: `test.email${item}@malahngoding.com`,
+            name: randomSlug(),
+            email: `${randomSlug()}@malahngoding.com`,
             avatar: `${randomstring.generate(14)}`,
             bio: `Created for Test with id ${item}`,
           },
